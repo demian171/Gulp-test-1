d648ae7bd68d0c0987c5e94d9e75efa9524e579b
Dmytro Bilyak <demian171@gmail.com>
2022-01-20 14:07:20 +0200

    add sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,35 +7,37 @@ const plumber = require('gulp-plumber');
 const concat = require('gulp-concat'); //конкатенирование файлов
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass')(require('sass'));
-var sourcemaps = require('gulp-sourcemaps');
-
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 const fileinclude = require('gulp-file-include'); //ипортирование частей
 
 
 const moveCSS = () => 
-    gulp.src("./src/css/*.css"). 
-    pipe(gulp.dest("./dist/css/"));
+    gulp.src("./src/css/*.css")
+    .pipe(gulp.dest("./dist/css/"))
+    .pipe(browserSync.stream());
 
 const moveIMG = () => 
-    gulp.src("./src/image/**/*.jpg"). 
-    pipe(gulp.dest("./dist/img/"));
+    gulp.src("./src/img/**")
+    .pipe(imagemin({
+        progressive: true
+    }))
+    .pipe(gulp.dest("./dist/img/"))
+    .pipe(browserSync.stream());
 
 const concatCss = () =>
     gulp.src('./src/css/*.css')
     .pipe(concat('all.css'))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.stream());
 
 const moveHtml = () =>
     gulp.src('./src/*.html')
     .pipe(fileinclude())
-    .pipe(gulp.dest('./dist/'));
-
-const buildStyles = () =>
-    gulp.src('./src/sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
-        //.pipe(sass())
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.stream());
 
 function buildScss() {
     return gulp.src('./src/scss/**/*.scss')
@@ -49,15 +51,15 @@ function buildScss() {
             cascade: false
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 };
-
 
 gulp.task("moveCSS", moveCSS);
 gulp.task("moveIMG", moveIMG);
 gulp.task("concatCss", concatCss);
 gulp.task("moveHtml", moveHtml);
-gulp.task("buildStyles", buildStyles);
+//gulp.task("buildStyles", buildStyles);
 gulp.task("buildScss", buildScss);
 
 // gulp.parallel принимает название функций, которые должны выполняться
@@ -65,25 +67,31 @@ gulp.task("buildScss", buildScss);
 
 // Если же названия указаны в кавычках, то это - название тасок. Если эти таски не было созданы, то команда gulp moveFiles вызовет  ошибку
 gulp.task("moveFiles", gulp.parallel("buildScss", "moveIMG", "moveHtml"));
-//gulp.task("moveFiles2", gulp.parallel(moveCSS, moveIMG));
-/*
-gulp.task("moveFiles2", ()=>{
-    moveCSS();
-    moveIMG();
+
+
+gulp.task('serve', () => {
+    return browserSync.init({
+        server: {
+            baseDir: [ 'dist' ]
+        },
+        port: 9000,
+        open: true
+    });
 });
-*/
+
+
 const watch = () => {
 	//gulp.watch('./src/css/*.css', concatCss);
     //gulp.watch('./src/css/*.css', moveCSS);
-    gulp.watch('./src/image/**/*.jpg', moveIMG);
+    gulp.watch('./src/img/**', moveIMG).on('change', browserSync.reload);
     //gulp.watch('./src/sass/**/*.sass', buildStyles);
-    gulp.watch('./src/scss/**/*.scss', buildScss);
-    gulp.watch('./src/**/*.html', moveHtml);
+    gulp.watch('./src/scss/**/*.scss', buildScss).on('change', browserSync.reload);
+    gulp.watch('./src/**/*.html', moveHtml).on('change', browserSync.reload);
 }
 
 const delDist = () => {
     return del('./dist');
 }
 
-gulp.task('default', gulp.series(delDist, 'moveFiles', watch));
+gulp.task('default', gulp.series(delDist, 'moveFiles', gulp.parallel('serve', watch)));
 
